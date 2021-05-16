8aff789fd441dc09ed00738e1fae4c3900021bbf
Sindhu Bhat <v-sindhubhat@microsoft.com>
2021-05-16 21:09:13 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
+import React,  { Component }  from 'react';
 import Header from "./Header";
+import './App.css';
+ 
+class App extends Component {
 
-function App() {
-  return (
-    <div>
-    <Header />
-    <button>Add</button>
-    <div>
-      <span>Name</span><br />
-      <span>Phone</span>
-    </div> 
+  render() {
+    let subscribers=[
+      {
+        'id':1,
+        'name':'Sindhu Bhat',
+        'phone':'8888888888'
+      },
+      {
+        'id':2,
+        'name':'Shilpa Bhat',
+        'phone':'9999999999'
+      }
+    ]
+
+    return (
+      <div className='component-container'>
+      <Header />
+      <div className='component-body-container'>
+        <button className='button'>ADD</button>
+
+        <div className='grid-container heading-container'>
+          <span className='grid-item item-heading'>Name</span>
+          <span className='grid-item item-heading'> Phone</span>     
+        </div>
+
+        {
+          subscribers.map(sub=>{
+            return <div className='grid-container' key={sub.id}>
+            <span className='grid-item'>{sub.name}</span>
+            <span className='grid-item'> {sub.phone}</span>     
+          </div>
+          })
+        }
+        
+     </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
